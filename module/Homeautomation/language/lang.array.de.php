aaf3f75e8d330fb0152226fe6fdb7cc2e6134174
Daniel Schwarz <d.schwarz85@gmail.com>
2015-02-08 20:23:10 +0100

    reimplementing lost changes

--- module/Homeautomation/language/lang.array.de.php
+++ module/Homeautomation/language/lang.array.de.php
@@ -1,4 +1,15 @@
 <?php
 return array(
-	'Sockets' => 'Funksteckdosen'
+	'Sockets' => 'Funksteckdosen',
+	'Label' => 'Beschreibung',
+	'Action' => 'Aktion',
+	'Off' => 'Aus',
+	'On' => 'An',
+	'Error' => 'Fehler',
+	'Socket status could not be changed.' => 'Die Funksteckdose konnte nicht geschaltet werden.',
+	'Request failed.' => 'Anfrage fehlgeschlagen.',
+	'Sysinfo' => 'Informationen',
+	'Temperature' => 'Temperatur',
+	'measuring...' => 'messen...',
+	'Temperature could not be measured.' => 'Die Temperatur konnte nicht gemessen werden.'
 );
---
+++ public/js/custom.js
@@ -0,0 +1,64 @@
+//Open safari webapp urls in app instead of browser
+(function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener("click",function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;"href"in d&&(d.href.indexOf("http")||~d.href.indexOf(e.host))&&(a.preventDefault(),e.href=d.href)},!1)}})(document,window.navigator,"standalone");
+
+//Custom functions
+$(function() {
+	//Sockets
+	$('#sockets button.btn').click(function() {
+		var buttonInfo = $(this).attr('id').split('_');
+		$.ajax({
+			url: ajaxUrl,
+			data: { mode: 'socket', socket: buttonInfo[1], status: buttonInfo[2] },
+			cache: false,
+			dataType: 'json'
+		}).done(function(data) {
+			if(data.success == true) {
+				$('#socket_' + buttonInfo[1] + '_off').removeClass('btn-primary').addClass('btn-default');
+				$('#socket_' + buttonInfo[1] + '_on').removeClass('btn-primary').addClass('btn-default');
+				$('#socket_' + buttonInfo[1] + '_' + buttonInfo[2]).removeClass('btn-default').addClass('btn-primary');
+			} else {
+				BootstrapDialog.show({
+					type: BootstrapDialog.TYPE_WARNING,
+					title: lang.error,
+					message: lang.errormessage,
+				}); 
+			}			
+		}).fail(function() {
+            BootstrapDialog.show({
+                type: BootstrapDialog.TYPE_WARNING,
+                title: lang.error,
+                message: lang.httperrormessage,
+            });     
+		});
+	});
+	
+	//Sysinfo
+	//TODO recover :(
+	$('#btn_temperature').click(function() {
+		var $btn = $(this).button('loading');
+		$.ajax({
+			url: ajaxUrl,
+			data: { mode: 'temperature' },
+			cache: false,
+			dataType: 'json'
+		}).done(function(data) {
+			if(data.success == true) {
+				$btn.html(data.temperature + '&deg;C');
+			} else {
+				BootstrapDialog.show({
+					type: BootstrapDialog.TYPE_WARNING,
+					title: lang.error,
+					message: lang.errormessage,
+				}); 
+			}
+			$btn.button('reset');
+		}).fail(function() {
+            BootstrapDialog.show({
+                type: BootstrapDialog.TYPE_WARNING,
+                title: lang.error,
+                message: lang.httperrormessage,
+            });
+            $btn.button('reset')
+		});
+	});
+});
